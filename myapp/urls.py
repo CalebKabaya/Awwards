af92c1d9173aae2dbac868acc74c2767b1b42d29
Moringa School <calebmutuku4@gmail.com>
2022-06-11 13:10:37 +0300

    added the logout url

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,9 +2,12 @@ from django.urls import re_path
 from . import views
 
 urlpatterns=[
-     re_path('^$',views.welcome,name='welcome'),
+    re_path('^$',views.welcome,name='welcome'),
     re_path('login/',views.signin,name='login'),
     re_path('register/',views.register,name='register'),
+    re_path('signout/',views.signout,name='signout'),
+    re_path('profile/',views.profile,name='profile'),
+
 
 ]
 
