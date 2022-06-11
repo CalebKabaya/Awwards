3e338a7f0605cbaaf8c1aafedadafc0820175470
Moringa School <calebmutuku4@gmail.com>
2022-06-11 18:49:29 +0300

    added the add post function

--- myapp/views.py
+++ myapp/views.py
@@ -3,11 +3,15 @@ from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
 from django.contrib.auth import login,authenticate,logout
 from .models import Profile,Post
+from .forms import PostForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 def welcome(request):
-    return render(request,'index.html')
+    all_post=Post.objects.all()
+
+    return render(request,'index.html',{"all_post":all_post})
 
 def signin(request):
     if request.method=="POST":
@@ -53,3 +57,39 @@ def signout(request):
     logout(request)
     messages.success(request,"You have logged out, we will be glad to have you back again")
     return redirect ("login")
+
+@login_required(login_url='/accounts/login/')
+def addpost(request):
+    if request.method=="POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post= form.save(commit=False)
+            post.user=request.user
+            post.save()
+            return redirect('/')
+
+    else:
+        form=PostForm()
+    try:
+        posts=Post.objects.all() 
+        posts=posts[::-1] 
+    except Post.DoesNotExist:
+        posts=None
+    return render(request,'newpost.html',{"form":form,"posts":posts}) 
+
+@login_required(login_url='/accounts/login/')
+def postproject(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.user=request.user
+            project.save()
+        return redirect('/')
+    else:
+        form = PostForm()
+    context = {
+        'form':form,
+    }
+    return render(request, 'newpost.html', context)
+
