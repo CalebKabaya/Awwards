1af485a10c0933c710c7544a64c0dadc2422eb25
Moringa School <calebmutuku4@gmail.com>
2022-06-11 11:57:40 +0300

    added the functions to login and register

--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.contrib.auth import login,authenticate,logout
+
 
 # Create your views here.
 
@@ -6,7 +10,36 @@ def welcome(request):
     return render(request,'index.html')
 
 def signin(request):
+    if request.method=="POST":
+        username=request.POST["username"]
+        password=request.POST["password"]
+
+        user=authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            messages.success(request,"You have successfuly loged in")
+            return redirect ("/")
     return render(request,'login.html')
 
 def register(request):
+    if request.method == "POST":
+        username=request.POST["username"]
+        first_name=request.POST["first_name"]
+        last_name=request.POST["last_name"]
+        email=request.POST["email"]
+        password1=request.POST["password1"]
+        password2=request.POST["password2"]
+
+        if password1 !=password2:
+            messages.error(request,'Password do not match')
+            return render('/register')
+        new_user=User.objects.create_user(
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            password=password1,
+        ) 
+        new_user.save() 
+        return render (request,'login.html')
     return render(request,'register.html')
