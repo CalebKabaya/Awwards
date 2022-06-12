801114f5d8860e354d4896d13f1b948339c1be97
Moringa School <calebmutuku4@gmail.com>
2022-06-12 23:45:38 +0300

    ft:edited fiels in the models

--- myapp/models.py
+++ myapp/models.py
@@ -53,37 +53,37 @@ class Post(models.Model):
     def search_projects(cls,title):
         return cls.objects.filter(title__icontains=title).all()    
 
-
 class Rating(models.Model):
-    rating=(
-        (1,'1'),
-        (2,'2'),
-        (3,'3'),
-        (4,'4'),
-        (5,'5'),
-        (6,'6'),
-        (7,'7'),
-        (8,'8'),
-        (9,'9'),
-        (10,'10'),
+    rating = (
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+        (6, '6'),
+        (7, '7'),
+        (8, '8'),
+        (9, '9'),
+        (10, '10'),
     )
-    design=models.IntegerField(choices=rating,default=0,blank=True)
-    usability=models.IntegerField(choices=rating,blank=True)
-    content=models.IntegerField(choices=rating,blank=True)
-    score=models.FloatField(default=0,blank=True)
-    usability_average=models.FloatField(default=0,blank=True)
-    desing_average=models.FloatField(default=0,blank=True)
-    content_average=models.FloatField(default=0,blank=True)
-    user =models.ForeignKey(User,on_delete=models.CASCADE,null=True,related_name='rater')
-    post=models.ForeignKey(Post,on_delete=models.CASCADE,null=True,related_name='ratings')
+
+    design = models.IntegerField(choices=rating, default=0, blank=True)
+    usability = models.IntegerField(choices=rating, blank=True)
+    content = models.IntegerField(choices=rating, blank=True)
+    score = models.FloatField(default=0, blank=True)
+    design_average = models.FloatField(default=0, blank=True)
+    usability_average = models.FloatField(default=0, blank=True)
+    content_average = models.FloatField(default=0, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name='rater')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='ratings', null=True)
 
     def save_rating(self):
         self.save()
+
     @classmethod
-    def get_ratings(cls,id):
-        ratings= Rating.onjects.filter(post_id=id).all()
+    def get_ratings(cls, id):
+        ratings = Rating.objects.filter(post_id=id).all()
         return ratings
+
     def __str__(self):
-        return f'{self.post}Rating'        
-
-
+        return f'{self.post} Rating'
