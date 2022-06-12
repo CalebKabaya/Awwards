21c75a325d6dc18638c16412dda11c20163f2613
Moringa School <calebmutuku4@gmail.com>
2022-06-12 14:38:19 +0300

    added the search url in urls

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,17 +5,25 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     re_path('^$',views.welcome,name='welcome'),
+    re_path('^$',views.index,name='index'),
+
     re_path('login/',views.signin,name='login'),
     re_path('register/',views.register,name='register'),
     re_path('signout/',views.signout,name='signout'),
     re_path('profile/',views.profile,name='profile'),
     re_path('addpost/',views.addpost,name='addpost'),
     re_path('new-project/', views.postproject, name='newproject'),
-    # re_path('<username>/profile', views.user_profile, name='userprofile'),
-    # re_path('profile/<username>/settings', views.update_profile, name='update'),
-    # re_path('user_profile/<username>/', views.user_profile, name='user_profile'),
     re_path('update', views.update_profile, name='update'),
     re_path('project/<post>', views.project, name='project'),
+    re_path('search/', views.search_project, name='search'),
+
+    # re_path('project/', views.project, name='project'),
+    # re_path(r'^project/(?P<post>\d+)?$', views.project, name='project'),  
+    # re_path('project/', views.project, name='project'),
+  
+
+
+
 
 
     
